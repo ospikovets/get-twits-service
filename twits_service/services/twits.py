b0173142254f1f6e9157bfc5606c92ed169b8835
Oleksandr Pikovets <opikovets@spscommerce.com>
2018-08-06 13:16:12 +0300

    Add twits service that handles app business logic

--- twits_service/services/twits.py
+++ twits_service/services/twits.py
@@ -1,3 +1,6 @@
+from urllib.parse import quote_plus
+
+
 class TwitsService:
 
     def __init__(self, api, twit_model):
@@ -8,5 +11,21 @@ class TwitsService:
     def get_twits_by_hashtag(self, hashtag, pages_limit=10):
         """Search Twits by the specified hashtag"""
 
+        query = f'#{hashtag}'
+        query = quote_plus(query)
+
+        search_results = self.api.search(query, count=pages_limit)
+        twit_dicts = search_results['statuses'][:pages_limit]
+
+        return list(map(self.twit_model.from_dict, twit_dicts))
+
     def get_twits_by_username(self, username, pages_limit=10):
         """Search Twits by the specified username"""
+
+        query = f'from:{username}'
+        query = quote_plus(query)
+
+        search_results = self.api.search(query, count=pages_limit)
+        twit_dicts = search_results['statuses'][:pages_limit]
+
+        return list(map(self.twit_model.from_dict, twit_dicts))
